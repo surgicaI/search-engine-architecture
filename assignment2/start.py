849a042d77eeeaef2873a8332d46430abfecde95
Simranjyot Singh <simranjyot1991@gmail.com>
2017-02-15 14:44:05 -0500

    correcting query syntax

--- assignment2/start.py
+++ assignment2/start.py
@@ -13,16 +13,14 @@ class DefaultHandler(tornado.web.RequestHandler):
 
 class MainHandler(tornado.web.RequestHandler):
     @gen.coroutine
-    def get(self,query):
-        #self.write(str(query))
-        #self.write(self.request.uri)
+    def get(self):
         dict = {}
         http_client = AsyncHTTPClient()
         frontend_server_response = {}
+        query = self.get_argument("q", "Default")
         for index_server in inventory.index_servers:
             index_server_response = yield http_client.fetch(index_server+"/index?q="+query)
             jsonResponse = json.loads(index_server_response.body.decode('utf-8')) 
-            #self.write(jsonResponse)
             for docIdScorePair in jsonResponse['postings']:
                 doc_id = docIdScorePair[0]
                 score = docIdScorePair[1]
@@ -38,6 +36,7 @@ class MainHandler(tornado.web.RequestHandler):
             doc_server_response = yield http_client.fetch(inventory.doc_servers[index]+"/doc?id="+str(doc_id)+"&q="+query)
             json_doc_server_response = json.loads(doc_server_response.body.decode('utf-8'))
             results = json_doc_server_response['results']
+            response['doc_id'] = doc_id
             response['title'] = results[0]['title']
             response['url'] = results[0]['url']
             response['snippet'] = results[0]['snippet']
@@ -48,6 +47,6 @@ class MainHandler(tornado.web.RequestHandler):
 
 
 if __name__ == "__main__":
-    app = tornado.web.Application([(r"/", DefaultHandler),(r"/searchq=([a-z]+)", MainHandler),])
+    app = tornado.web.Application([(r"/", DefaultHandler),(r"/search", MainHandler),])
     app.listen(inventory.BASE_PORT)
     tornado.ioloop.IOLoop.current().start()
