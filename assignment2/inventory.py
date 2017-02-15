06d96c952e8b61bbd822b59131b2c405de047fe0
Simranjyot Singh <simranjyot1991@gmail.com>
2017-02-15 08:18:56 -0500

    implements front end server

--- assignment2/inventory.py
+++ assignment2/inventory.py
@@ -3,6 +3,8 @@ BASE_PORT = 55700
 index_partitions = 3
 document_partitions = 3
 
+items_to_display = 10
+
 index_servers = ["http://linserv2.cims.nyu.edu:35315","http://linserv2.cims.nyu.edu:35316","http://linserv2.cims.nyu.edu:35317"]
 doc_servers = ["http://linserv2.cims.nyu.edu:35318","http://linserv2.cims.nyu.edu:35319","http://linserv2.cims.nyu.edu:35320"]
 frontend_port = "http://linserv2.cims.nyu.edu:"+str(BASE_PORT)
--- assignment2/start.py
+++ assignment2/start.py
@@ -4,15 +4,50 @@ from tornado.httpclient import AsyncHTTPClient
 from tornado import gen
 import socket
 import inventory
+import json
+import operator
 
+class DefaultHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello World!")
 
 class MainHandler(tornado.web.RequestHandler):
     @gen.coroutine
-    def get(self):
-        #response = yield http_client.fetch("http://linserv2.cims.nyu.edu:"+str(backendPorts[currentPortIndex]))
-        self.write("Hello World")
+    def get(self,query):
+        #self.write(str(query))
+        #self.write(self.request.uri)
+        dict = {}
+        http_client = AsyncHTTPClient()
+        frontend_server_response = {}
+        for index_server in inventory.index_servers:
+            index_server_response = yield http_client.fetch(index_server+"/index?q="+query)
+            jsonResponse = json.loads(index_server_response.body) 
+            #self.write(jsonResponse)
+            for docIdScorePair in jsonResponse['postings']:
+                doc_id = docIdScorePair[0]
+                score = docIdScorePair[1]
+                dict[doc_id] = score
+
+        sortedList = sorted(dict.items(), key=operator.itemgetter(1))
+        sortedList.reverse()
+        sortedList = sortedList[0:inventory.items_to_display]
+        frontend_response_list = []
+        for doc_id,score in sortedList:
+            response = {}
+            index = doc_id%len(inventory.doc_servers)
+            doc_server_response = yield http_client.fetch(inventory.doc_servers[index]+"/doc?id="+str(doc_id)+"&q="+query)
+            json_doc_server_response = json.loads(doc_server_response.body)
+            results = json_doc_server_response['results']
+            response['title'] = results[0]['title']
+            response['url'] = results[0]['url']
+            response['snippet'] = results[0]['snippet']
+            frontend_response_list.append(response)
+        frontend_server_response['num_results'] = len(frontend_response_list)
+        frontend_server_response['results'] = frontend_response_list
+        self.write(json.dumps(frontend_server_response))
+
 
 if __name__ == "__main__":
-    app = tornado.web.Application([(r"/", MainHandler),])
+    app = tornado.web.Application([(r"/", DefaultHandler),(r"/searchq=([a-z]+)", MainHandler),])
     app.listen(inventory.BASE_PORT)
     tornado.ioloop.IOLoop.current().start()
