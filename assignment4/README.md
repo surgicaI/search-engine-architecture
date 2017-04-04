f58e5be7aa39a63653cd10e4f49ceafd8a227045
Simranjyot Singh <simranjyot1991@gmail.com>
2017-04-04 00:46:07 -0400

    making number of index partitions and document partitions coherent. Also updating readme

--- assignment4/README.md
+++ assignment4/README.md
@@ -2,12 +2,14 @@
 * Run the code on linserv2.
 * After loggin in on linserv2, use command "module load python-3.5.2"
 * To create .in files for all MapReduce programs use following command in sea-assignments directory ie root folder.
-    &nbsp;&nbsp; ./assignment4/reformat_all.sh
+    ./assignment4/reformat_all.sh
 * To start up MapReduce framework use following command in root folder.
-    &nbsp;&nbsp; python -m assignment3.workers
+    python -m assignment3.workers
 * To run three MapReduce jobs and generate all of the index files, use following command in root folder.
-    &nbsp;&nbsp; python -m assignment4.start
+    python -m assignment4.start
 * To start up the search engine, use following command in root folder.
-    &nbsp;&nbsp; python -m assignment2.start
+    python -m assignment2.start
 * Use the following url in browser to test the results
-    &nbsp;&nbsp; http://linserv2.cims.nyu.edu:55700/search?q=personalized
+    http://linserv2.cims.nyu.edu:55700/search?q=personalized
+* To change the number of index or document partitions, change in assignment2.inventory
+* To change the number of workers for map reduce, change in assignment3.inventory
--- assignment4/start.py
+++ assignment4/start.py
@@ -1,18 +1,19 @@
 import subprocess
+import assignment2.inventory as inventory
 
 env = 'python'
 
 #for inverted index
 subprocess.Popen([env, '-m', 'assignment3.coordinator', '--mapper_path=assignment4/mr_apps/invindex_mapper.py',
     '--reducer_path=assignment4/mr_apps/invindex_reducer.py', '--job_path=assignment4/invindex_jobs',
-    '--num_reducers=3'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    '--num_reducers='+str(inventory.index_partitions)], stdout=subprocess.DEVNULL)
 
 #for document indexes
 subprocess.Popen([env, '-m', 'assignment3.coordinator', '--mapper_path=assignment4/mr_apps/docs_mapper.py',
     '--reducer_path=assignment4/mr_apps/docs_reducer.py', '--job_path=assignment4/docs_jobs',
-    '--num_reducers=3'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    '--num_reducers='+str(inventory.document_partitions)], stdout=subprocess.DEVNULL)
 
 #for idf
 subprocess.Popen([env, '-m', 'assignment3.coordinator', '--mapper_path=assignment4/mr_apps/idf_mapper.py',
     '--reducer_path=assignment4/mr_apps/idf_reducer.py', '--job_path=assignment4/idf_jobs',
-    '--num_reducers=3'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    '--num_reducers=1'], stdout=subprocess.DEVNULL)
