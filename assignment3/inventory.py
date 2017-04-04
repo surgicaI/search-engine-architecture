3ddc84f1ecc3f044ad4bbc240e56c0a9f0aca8b8
Simranjyot Singh Gill <ssg441@linserv2.cims.nyu.edu>
2017-04-04 01:00:28 -0400

    correcting syntax error

--- assignment3/inventory.py
+++ assignment3/inventory.py
@@ -11,7 +11,7 @@ worker_ports = [55701,55702,55703,55704,55705,55706,55707,55708,55709,55710]
 worker_servers = ["linserv2.cims.nyu.edu:55701","linserv2.cims.nyu.edu:55702","linserv2.cims.nyu.edu:55703",
 "linserv2.cims.nyu.edu:55704","linserv2.cims.nyu.edu:55705","linserv2.cims.nyu.edu:55706",
 "linserv2.cims.nyu.edu:55707","linserv2.cims.nyu.edu:55708","linserv2.cims.nyu.edu:55709",
-"linserv2.cims.nyu.edu:55710"
+"linserv2.cims.nyu.edu:55710"]
 
 '''#local servers
 worker_servers = ["127.0.0.1:55701","127.0.0.1:55702","127.0.0.1:55703","127.0.0.1:55704",
